260395944fd0d15e94554ddd92157f4ddee18c62
yoshimo <109893020+yoshi-mo@users.noreply.github.com>
2023-06-18 18:25:39 +0900

    fixed not found page

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/App.css"
 
 const NotFound = () => {
+
     return (
         <div className="not-found">
             <div className="content">
