c20972adf688d6b962114bdaaf2be94aeea01fb2
yoshimo <109893020+yoshi-mo@users.noreply.github.com>
2023-06-18 17:52:26 +0900

    not found page was finished

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import "../styles/App.css"
 
 const NotFound = () => {
-    return <div>NotFound</div>;
+    return (
+        <div className="not-found">
+            <div className="content">
+                <h3>ページが見つかりませんでした</h3>
+                <p>404 - Not Found</p>
+            </div>
+        </div>
+    );
 };
 
 export default NotFound;
